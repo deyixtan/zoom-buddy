7fb4b75baae3270bc271c153b1ca9f2b05eb001d
Deyi Tan <deyixtan@gmail.com>
2022-01-09 03:37:59 +0800

    Update entry point to wrap injection calls in a method

--- frontend/entry.js
+++ frontend/entry.js
@@ -1,14 +1,14 @@
 /**
  * Entry point into the extension.
  * Injects all required stylesheets and scripts.
- * 
+ *
  * More info as to why this injectino procedure was used:
  * http://stackoverflow.com/questions/20499994/access-window-variable-from-content-script}
- */ 
+ */
 
 const injectScript = (file_path, isExtensionResource) => {
   if (isExtensionResource) {
-    file_path = chrome.extension.getURL(file_path)
+    file_path = chrome.extension.getURL(file_path);
   }
 
   const body = document.getElementsByTagName("body")[0];
@@ -16,11 +16,11 @@ const injectScript = (file_path, isExtensionResource) => {
   script.setAttribute("type", "text/javascript");
   script.setAttribute("src", file_path);
   body.appendChild(script);
-}
+};
 
 const injectStylesheet = (file_path, isExtensionResource) => {
   if (isExtensionResource) {
-    file_path = chrome.extension.getURL(file_path)
+    file_path = chrome.extension.getURL(file_path);
   }
 
   const head = document.getElementsByTagName("head")[0];
@@ -28,11 +28,15 @@ const injectStylesheet = (file_path, isExtensionResource) => {
   stylesheet.setAttribute("rel", "stylesheet");
   stylesheet.setAttribute("href", file_path);
   head.appendChild(stylesheet);
-}
+};
 
-// Inject stylesheets
-injectStylesheet("bootstrap.min.css", true);
+const main = () => {
+  // Inject stylesheets
+  injectStylesheet("bootstrap.min.css", true);
 
-// Inject scripts
-injectScript("socket.io.min.js", true);
-injectScript("content-script.js", true);
+  // Inject scripts
+  injectScript("socket.io.min.js", true);
+  injectScript("content-script.js", true);
+};
+
+main();
